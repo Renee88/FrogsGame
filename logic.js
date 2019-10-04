1ac8d5914ab2b6d4efdaf1623ba57a64335fbfc0
Your Name <you@example.com>
2019-10-04 12:10:00 +0300

    Fixed the Random color and random size properties

--- logic.js
+++ logic.js
@@ -1,44 +1,58 @@
-Frogies = function(){
+Frogies = function () {
     let frogs = []
-    let frogsNum = frogs.length
 
-    const getFrogs = function(){
+    const getFrogs = function () {
         return frogs
     }
 
-    const addFrog = function(){  // Triggered when pressing the Start button
+    const addFrog = function () {  // Triggered when pressing the Start button
         let frog = {}
         frog.color = randomColor()
         randomLocation(frog)
-        frogSize(frog.top,frog)
+        frogSize(frog.top, frog)
         frogs.push(frog)
         return frog
     }
 
-    const removeFrog = function(frogIndex){
+    const removeFrog = function (frogIndex) {
         frogs.splice(frogIndex, 1)
     }
 
-    const randomColor = function(){  // Random color for each frog
+    const randomColor = function () {  // Random color for each frog
         const letters = '0123456789ABCDEF';
         let color = '#';
         for (var i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
+            color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
     }
 
-    const randomLocation = function(frog){    // Adds left and top values to locate the frog randomly and relatively to the parent div
-         leftNum = Math.floor(Math.random()*100)
-         frog.left = leftNum
-         topNum = Math.floor(Math.random()*100)
-         frog.top = topNum
+    const randomLocation = function (frog) {    // Adds left and top values to locate the frog randomly and relatively to the parent div
+        leftNum = Math.floor(Math.random() * 100)
+        frog.left = leftNum
+        topNum = Math.floor(Math.random() * 100)
+        frog.top = topNum
     }
 
-    const frogSize = function(top, frog){  // gets the top key value (not a percentage) and sets the font size
+    const frogSize = function (top, frog) {  // gets the top key value (not a percentage) and sets the font size
         let size = 100 - top
         frog.size = size
+        if (size < 16) {
+            frog.size = 16
+        }
     }
+
+    const countdown = function (seconds) {
+
+        setInterval(() => {
+            if (seconds > 0) {
+                seconds -= 1
+            }
+        }, 1000)
+
+    }
+
+
 
     return {
         getFrogs,
@@ -46,14 +60,15 @@ Frogies = function(){
         removeFrog,
         randomColor,
         randomLocation,
-        frogSize
+        frogSize,
+        countdown
     }
-
 }
 
-frogies = Frogies()
-frogs = frogies.getFrogs()
-frog = frogies.addFrog()
+let frogies = Frogies()
+
+frogies.addFrog()
+
 
 
 
--- render.js
+++ render.js
@@ -1,45 +1,50 @@
-Renderer = function() {
-    let frogCounter = 0
-    addFrogDiv = function(frogs){
-        for (let frog of frogs){
-        const frogSize = getSize(frog)
-        fontSize = frogSize + "px"
-        frogCounter +=1
-        frogColor = getColor(frog)
-        let top = frog.top + "px"
-        let left = frog.left + "px"
-        $("#game").append(`<div class = frog data-id = f${frogCounter}><i class="fas fa-frog"></i></div>`)
-        $(`[data-id=f${frogCounter}]`).css("font-size",fontSize)
-        $(`[data-id=f${frogCounter}]`).css("top", top )
-        $(`[data-id=f${frogCounter}]`).css("left", left )
+Renderer = function () {
+
+    addFrogDiv = function (frogs) {
+        let frogCounter = 0
+        for (let frog of frogs) {
+            const frogSize = getSize(frog)
+            fontSize = frogSize 
+            frogCounter += 1
+            frogColor = getColor(frog)
+            let top = frog.top 
+            let left = frog.left 
+            $("#game").append(`<div data-id = box-${frogCounter} position:relative><i class="fas fa-frog" data-id =frog-${frogCounter}></i></div>`)
+            $(`[data-id=frog-${frogCounter}]`).css("color",frogColor)
+            $(`[data-id=frog-${frogCounter}]`).css("fontSize", fontSize )
+            $(`[data-id=frog-${frogCounter}]`).offset("position","absolute")
+            $(`[data-id=frog-${frogCounter}]`).css({ left: 20 , top: 30 })
+
         }
-        
     }
-    removeGo = function(){
+    const removeGo = function () {
         $("#go").remove()
     }
 
-    getSize = function(frog){
+    const getSize = function (frog) {
         return frog.size
     }
 
-    getColor = function(frog){
+    const getColor = function (frog) {
         return frog.color
     }
 
-    countDown = function(){
+    const timerDiv = function () {
 
     }
 
-    return { removeGo,
-             addFrogDiv,
-            }
+    return {
+        removeGo,
+        addFrogDiv
+    }
+
 }
 
 const render = Renderer()
 let frogs = frogies.getFrogs()
-let frog = frogies.addFrog()
-render.addFrogDiv(frogs)
+console.log(frogs)
 render.removeGo()
+render.addFrogDiv(frogs)
+
 
 
